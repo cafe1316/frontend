d31dc4045ef57eb8cfc03c58dbd5aa0f9cb3273c
zianwang <913248383@qq.com>
2025-09-01 22:17:23 +1000

    feat: learn

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { signInWithRedirect, getRedirectResult } from "firebase/auth";
-import { auth, provider } from "../../firebase"; // 注意你的路径
+import { auth, provider } from "../../lib/firebase"; // 注意你的路径
 import { useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
